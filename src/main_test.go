b672ddca1d229e8892411e884907fdf31e59ba49
sahu04 <105695204+sahu04@users.noreply.github.com>
2024-02-16 20:07:40 +0530

    Update main_test.go

--- src/main_test.go
+++ src/main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	total := sum(5, 5)
+	total := Sum(5, 5)
 	if total != 10 {
 		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
 	}
