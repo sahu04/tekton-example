00102a4216b1a1f1f3c10e3ea3279da7e9ccc1be
sahu04 <105695204+sahu04@users.noreply.github.com>
2024-02-16 20:07:59 +0530

    Update main.go

--- src/main.go
+++ src/main.go
@@ -2,11 +2,10 @@ package main
 
 import "fmt"
 
-// Sum calculates the sum of two integers
 func Sum(a, b int) int {
-	return a + b
+    return a + b
 }
 
 func main() {
-	fmt.Println("Sum:", Sum(1, 2))
+    fmt.Println("Sum:", Sum(1,2))
 }
