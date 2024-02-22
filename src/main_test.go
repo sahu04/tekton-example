79c26611546e7b2bdb6d675c23c4b2c46492fd3c
sahu04 <105695204+sahu04@users.noreply.github.com>
2024-02-22 11:42:34 +0530

    Update main_test.go

--- src/main_test.go
+++ src/main_test.go
@@ -1,10 +1,13 @@
-package main
+package greeting
 
 import "testing"
 
-func TestSum(t *testing.T) {
-	total := Sum(5, 5)
-	if total != 10 {
-		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
+func TestAdd(t *testing.T) {
+	if ans := Add(1, 2); ans != 3 {
+		t.Errorf("1 + 2 expected be 3, but %d got", ans)
+	}
+
+	if ans := Add(-10, -20); ans != -30 {
+		t.Errorf("-10 + -20 expected be -30, but %d got", ans)
 	}
 }
